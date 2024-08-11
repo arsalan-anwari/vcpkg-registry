d748fc209d670e28701ecbee925e4187eeb38923
arsalan-anwari <arsalan.anwari@outlook.comm>
2024-08-11 16:01:55 +0200

    Changed portfile to add safety guards

--- ports/nvapi/portfile.cmake
+++ ports/nvapi/portfile.cmake
@@ -6,37 +6,61 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-# Install headers
-file(INSTALL
-  DESTINATION "${CURRENT_PACKAGES_DIR}/include"
-  FILES
-    ${SOURCE_PATH}/nvapi.h
-    ${SOURCE_PATH}/NvApiDriverSettings.h
-    ${SOURCE_PATH}/nvapi_interface.h
-    ${SOURCE_PATH}/nvapi_lite_common.h
-    ${SOURCE_PATH}/nvapi_lite_d3dext.h
-    ${SOURCE_PATH}/nvapi_lite_salend.h
-    ${SOURCE_PATH}/nvapi_lite_salstart.h
-    ${SOURCE_PATH}/nvapi_lite_sli.h
-    ${SOURCE_PATH}/nvapi_lite_stereo.h
-    ${SOURCE_PATH}/nvapi_lite_surround.h
-    ${SOURCE_PATH}/nvHLSLExtns.h
-    ${SOURCE_PATH}/nvHLSLExtnsInternal.h
-    ${SOURCE_PATH}/nvShaderExtnEnums.h
+# Ensure destination directories exist
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/include")
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/lib")
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/nvapi/")
+
+# Install headers if they exist
+if(EXISTS "${SOURCE_PATH}/nvapi.h")
+  file(INSTALL
+    DESTINATION "${CURRENT_PACKAGES_DIR}/include"
+    FILES
+      ${SOURCE_PATH}/nvapi.h
+      ${SOURCE_PATH}/NvApiDriverSettings.h
+      ${SOURCE_PATH}/nvapi_interface.h
+      ${SOURCE_PATH}/nvapi_lite_common.h
+      ${SOURCE_PATH}/nvapi_lite_d3dext.h
+      ${SOURCE_PATH}/nvapi_lite_salend.h
+      ${SOURCE_PATH}/nvapi_lite_salstart.h
+      ${SOURCE_PATH}/nvapi_lite_sli.h
+      ${SOURCE_PATH}/nvapi_lite_stereo.h
+      ${SOURCE_PATH}/nvapi_lite_surround.h
+      ${SOURCE_PATH}/nvHLSLExtns.h
+      ${SOURCE_PATH}/nvHLSLExtnsInternal.h
+      ${SOURCE_PATH}/nvShaderExtnEnums.h
+  )
+else()
+  message(WARNING "Header files not found in ${SOURCE_PATH}")
+endif()
+
+# Install library file if it exists
+if(EXISTS "${SOURCE_PATH}/amd64/nvapi64.lib")
+  file(INSTALL
+    DESTINATION "${CURRENT_PACKAGES_DIR}/lib"
+    FILES
+      ${SOURCE_PATH}/amd64/nvapi64.lib
+  )
+else()
+  message(WARNING "Library file not found in ${SOURCE_PATH}/amd64")
+endif()
+
+# Create CMake configuration files
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/nvapi/nvapiConfig.cmake"
+  "include(CMakeFindDependencyMacro)\n"
+  "find_dependency(PkgConfig)\n"
+  "include(${CMAKE_CURRENT_LIST_DIR}/nvapiTargets.cmake)\n"
 )
 
-# Install the .lib file
-file(INSTALL
-  DESTINATION "${CURRENT_PACKAGES_DIR}/lib"
-  FILES
-    ${SOURCE_PATH}/amd64/nvapi64.lib
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/nvapi/nvapiTargets.cmake"
+  "include(CMakeFindDependencyMacro)\n"
+  "include(CMakePackageConfigHelpers)\n"
+  "include(CMakeFindDependencyMacro)\n"
+  "set_and_check(NVAPI_INCLUDE_DIRS \"${CURRENT_PACKAGES_DIR}/include\")\n"
+  "set_and_check(NVAPI_LIBRARIES \"${CURRENT_PACKAGES_DIR}/lib/nvapi64.lib\")\n"
+  "add_library(nvapi::nvapi UNKNOWN IMPORTED)\n"
+  "set_target_properties(nvapi::nvapi PROPERTIES\n"
+  "  INTERFACE_INCLUDE_DIRECTORIES \"${CURRENT_PACKAGES_DIR}/include\"\n"
+  "  IMPORTED_LOCATION \"${CURRENT_PACKAGES_DIR}/lib/nvapi64.lib\"\n"
+  ")\n"
 )
-
-# Set up the CMake configuration
-vcpkg_cmake_configure(
-  SOURCE_PATH ${SOURCE_PATH}
-  PREFER_NINJA
-)
-
-vcpkg_cmake_build()
-vcpkg_cmake_install()
