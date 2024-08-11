f8749367cf22143dbc476811e0b9cf6548714747
arsalan-anwari <arsalan.anwari@outlook.comm>
2024-08-11 21:25:42 +0200

    Remove use of install function and used file to fix subscriptable error in cmake. Also supressed install messages

--- ports/nvapi/portfile.cmake
+++ ports/nvapi/portfile.cmake
@@ -8,7 +8,7 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-# Supress all install messages
+# Supress install and copy messages temporariliy
 set(CMAKE_INSTALL_MESSAGE NEVER)
 
 # Ensure destination directories exists
@@ -18,23 +18,26 @@ file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/include/nvapi")
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/nvapi")
 
 # Add library information files
-install(FILES "${PORT_PATH}/copyright" DESTINATION "${CURRENT_PACKAGES_DIR}/share/nvapi")
-install(FILES "${PORT_PATH}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/nvapi")
-install(FILES "${PORT_PATH}/nvapi.pc" DESTINATION "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/copyright" DESTINATION "${CURRENT_PACKAGES_DIR}/share/nvapi")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/nvapi")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/nvapi.pc" DESTINATION "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
 
-# Find all header files in the root directory
-file(GLOB_RECURSE NVAPI_HEADERS
+# Copy CMake configuration files
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/nvapi-targets.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/nvapi")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/nvapi-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/nvapi")
+
+# Find all header files in the root directory only
+file(GLOB NVAPI_HEADERS
     LIST_DIRECTORIES false
     "${SOURCE_PATH}/*.h"
     "${SOURCE_PATH}/*.hpp"
 )
 
 # Copy headers to include folder
-install(FILES ${NVAPI_HEADERS} DESTINATION "${CURRENT_PACKAGES_DIR}/include/nvapi")
+file(COPY ${NVAPI_HEADERS} DESTINATION "${CURRENT_PACKAGES_DIR}/include/nvapi")
 
 # Copy library to lib folder
-install(FILES "${SOURCE_PATH}/amd64/nvapi64.lib" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+file(COPY "${SOURCE_PATH}/amd64/nvapi64.lib" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
 
-# Copy CMake configuration files
-install(FILES "${PORT_PATH}/nvapi-targets.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/nvapi")
-install(FILES "${PORT_PATH}/nvapi-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/nvapi")
+# Re-enable install and copy messages again
+set(CMAKE_INSTALL_MESSAGE ALWAYS)
