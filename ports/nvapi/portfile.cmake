8db456aa6c063aad28cb0be0a8b2201d588a3dde
arsalan-anwari <arsalan.anwari@outlook.comm>
2024-08-11 14:37:48 +0200

    updated portfile to install nvapi

--- ports/nvapi/portfile.cmake
+++ ports/nvapi/portfile.cmake
@@ -3,16 +3,20 @@ vcpkg_from_github(
     REPO NVIDIA/nvapi
     REF 4ba3384657149d63aa193f5a34e20efe1e42bf31
     SHA512 F8A1E3BCC2EB71D4020347B5B950C1376C82724915C587262B2FAAB1D8AEB1D81DA24827C11C64DD4CDF3C93076CAB24E45F675C4C6168510A06D1D561CD2A5C
+    HEAD_REF master
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
-    -DBUILD_SHARED_LIBS=ON
-    -DNVAPI_BUILD_TESTS=OFF
-)
+# Define the installation directories
+set(INSTALL_DIR ${SOURCE_PATH}/amd64)
+set(INCLUDE_DIR ${SOURCE_PATH})
 
-vcpkg_cmake_build()
+# Glob all header files in the root directory
+file(GLOB HEADER_FILES "${INCLUDE_DIR}/*.h")
 
-vcpkg_cmake_install()
+# Copy all header files to the include directory in Vcpkg
+foreach(header_file ${HEADER_FILES})
+    file(COPY ${header_file} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+endforeach()
+
+# Copy the library file
+file(COPY ${INSTALL_DIR}/nvapi64.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
